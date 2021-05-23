ef8474eea940caa842d7d542323a2dd48596b517
Gyordan Caminati <gyordan.caminati@studio.unibo.it>
2021-05-23 18:01:25 +0200

    ok

--- code/Caminati/ActorController.scala
+++
@@ -1,4 +0,0 @@
-override def sendToLobbyStrategy[T](strategy: MultiPlayerSetup => T): T = strategy.apply(matchSetupMp.get)
-
-override def sendToViewStrategy(strategy: ObserverUI => Unit): Unit =
-  observers.foreach(obs => Platform.runLater(() => strategy(obs)))
--- code/Giulianini/event.scala
+++
@@ -1,9 +0,0 @@
-sealed trait Event
-object Event {
-  sealed trait DisplayableEvent extends Event
-  final case class DrawEntityEvent(entity: Seq[Entity]) extends DisplayableEvent
-  final case class LevelSetupEvent(data: LevelSetupData) extends DisplayableEvent
-  final case class LevelEndEvent(data: LevelEndData) extends DisplayableEvent
-  sealed trait CommandableEvent extends Event
-  final case class CommandEvent(cmd: CommandData) extends CommandableEvent
-}
